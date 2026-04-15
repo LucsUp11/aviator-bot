9d4f44c542e34459f258ab034a2852ead217908a
LucsUp11 <luscasup11@gmail.com>
2026-04-15 18:36:29 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import requests
+import time
 
+TOKEN = "8781088670:AAEsHrAu6y7z2VNfyWU-NZeAwjLpTywfB7A"
+CHAT_ID = "1545696519"
+
+def enviar_sinal():
+    mensagem = "🚀 SINAL AVIATOR DETECTADO\n💰 Entrar agora\n🎯 Saída 2.00x"
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": mensagem
+    })
+
+while True:
+    enviar_sinal()
+    time.sleep(300)
